78ddff419dd12a51d2485a4f40268857d163c1ca
liorkesten <kestenlior@gmail.com>
2020-07-23 13:41:15 +0300

    Update Algorithms

--- Algorithms/SearchInArray.py
+++ Algorithms/SearchInArray.py
@@ -56,6 +56,8 @@ def findMed(A):
 def quick_select(A, k, s=0, e=-1):
     """
     Find the k^th element in A. using partition.
+                    Time Complexity : Worst case - O(n^2)
+                                        Avg case: - O(n)
     :param A: Array of numbers.
     :param k: The k^th element
     :param s: Start index
@@ -87,3 +89,23 @@ def findMax(A):
             maximum = item
 
     return maximum
+
+
+def is_arithmetic_progression(lst):
+    """
+    Check if there is a 3 numbers that are a is_arithmetic_progression.
+    for example -  [9,4,1,2] return False because there is not a sequence.
+                    [4,2,7,1] return True because there is 1,4,7 are sequence
+
+    :param lst: lst of diff integers
+    :return: True or False if there is a 3 sequence in the lst.
+    """
+    set_of_values = set(lst)
+    # Check if there is 3 monotony
+    for i in range(len(lst)):
+        for j in range(i + 1, len(lst)):
+            third_num = abs(lst[i] - lst[j]) + max(lst[i], lst[j])
+            if third_num in set_of_values:
+                return True
+
+    return False
---
+++ Data_Structures/DynamicList.py
@@ -0,0 +1,20 @@
+class DynamicList:
+    def __init__(self):
+        self.A = []
+        self.B = [] * 2
+
+    def insert(self):
+        # TODO
+        pass
+
+    def delete(self):
+        # TODO
+        pass
+
+    def get(self, i):
+        # TODO
+        pass
+
+    def set(self, i):
+        # TODO
+        pass
