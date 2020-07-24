e43a4b29c421fa958774b818ecf9c9793d3b6b81
liorkesten <kestenlior@gmail.com>
2020-07-25 02:02:35 +0300

    Update Algorithms

--- Algorithms/Strings_Algorithms/parenthesesChecker.py
+++ Algorithms/Strings_Algorithms/parenthesesChecker.py
@@ -1,33 +1,11 @@
 from Data_Structures.MyStack import MyStack
 
-NUM_OF_CHARS_LOWER_CASE = 26
-ASCII_NUM_FIRST_CHAR = ord("a")
 PARENTHESES = {
     '{': '}',
     '(': ')',
     '[': ']',
 }
 
-
-def areAnagrams(s, t):
-    """
-    Check if two strings are anagrams to each other
-    :param s: First string
-    :param t: Second string
-    :return: True or False if the strings are anagrams
-    """
-    if len(s) != len(t):
-        return False
-
-    char_counter = [0] * NUM_OF_CHARS_LOWER_CASE
-    try:
-        for i in range(len(s)):
-            char_counter[ord(s[i]) - ASCII_NUM_FIRST_CHAR] += 1
-            char_counter[ord(t[i]) - ASCII_NUM_FIRST_CHAR] -= 1
-    except IndexError:
-        raise Exception("The string must be string of lower case")
-
-    return all([c == 0 for c in char_counter])
 
 
 def parenthesesChecker(s):
